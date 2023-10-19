8a0c24bc767e099c5bf700e8f6b8215923fc9690
alexandre <alexandre.coutinho81@hotmail.com>
2023-10-19 09:55:05 -0300

    primeiro código gerado

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log("olá mundo")
+//VARIÁVEIS
+
+let heroi = ""
+
+let xpInicial = 0
+
+let xpConquistado = 0
+
+//OPERADORES
+
+let xp = xpInicial + xpConquistado
+
+
+//ESTRUTURA DE REPETIÇÃO
+
+if(heroi.length === 0){
+    console.log("Escolha um herói.")
+}
+
+
+//LAÇO DE REPETIÇÃO
+
+switch(true){
+    case xp > 10000:
+        console.log("XP do " + heroi + " é " + xp + " [Radiante]")
+        break
+
+    case xp > 9000:
+        console.log("XP do " + heroi + " é " + xp + " [Imortal]")
+        break
+
+    case xp > 8000:
+        console.log("XP do " + heroi + " é " + xp + " [Ascendente]")
+        break
+    
+    case xp > 7000:
+        console.log("XP do " + heroi + " é " + xp + " [Platina]")
+        break
+
+    case xp > 5000:
+        console.log("XP do " + heroi + " é " + xp + " [Ouro]")
+        break
+
+    case xp > 2000:
+        console.log("XP do " + heroi + " é " + xp + " [Prata]")
+        break
+
+    case xp > 1000:
+        console.log("XP do " + heroi + " é " + xp + " [Bronze]")
+        break
+
+    case xp === 0:
+        console.log("Inicie o jogo.")
+        break
+
+    default:
+        console.log("XP do " + heroi + " é " + xp + " [Ferro]")
+}
